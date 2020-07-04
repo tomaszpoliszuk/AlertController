aeb6bf7e8e6fa41d1c8eee540eeacaccd6a68982
Tomasz Poliszuk <tomaszpoliszuk@users.noreply.github.com>
2020-07-04 09:19:55 +0200

    Updated code for test alerts

--- AlertControllerSettings/AlertControllerSettings.mm
+++ AlertControllerSettings/AlertControllerSettings.mm
@@ -25,16 +25,30 @@
 	return _specifiers;
 }
 
-- (void)showExampleAlert {
-	UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"(V)(;,;)(V)" message:@"Why Not Zoidberg?" preferredStyle:UIAlertControllerStyleAlert];
+- (void) showExampleAlert {
+	UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Title of Example Alert" message:@"This is message of example Alert feel free to ignore what is written here, more words to make it a litle bit longer. \n All glory Hypnotoad." preferredStyle:UIAlertControllerStyleAlert];
 	[alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
 	[alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
 	[self presentViewController:alert animated:YES completion:nil];
 }
-- (void)showExampleActionSheet {
-	UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:@"( ͡° ͜ʖ ͡°)" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
-	[actionSheet addAction:[UIAlertAction actionWithTitle:@"(╯°益°)╯彡┻━┻" style:UIAlertActionStyleDefault handler:nil]];
-	[actionSheet addAction:[UIAlertAction actionWithTitle:@"( ͡° ͜ʖ ͡°) ( ͡⊙ ͜ʖ ͡⊙) ( ͡◉ ͜ʖ ͡◉ )" style:UIAlertActionStyleDefault handler:nil]];
+- (void) showExampleTextAlert {
+	UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Title of Example Text Alert" message:@"This is example of Alert with ability to type text in it (like login alerts)." preferredStyle:UIAlertControllerStyleAlert];
+	[alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+		textField.placeholder = @"Username";
+	}];
+	[alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+		textField.placeholder = @"Password";
+		textField.secureTextEntry = YES;
+	}];
+	[alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+	[alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+	[self presentViewController:alert animated:YES completion:nil];
+}
+- (void) showExampleActionSheet {
+	UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:@"Title of Example Action Sheet" message:@"This is message of example Action Sheet feel free to ignore what is written here, more words to make it a litle bit longer." preferredStyle:UIAlertControllerStyleActionSheet];
+	[actionSheet addAction:[UIAlertAction actionWithTitle:@"(•_•)" style:UIAlertActionStyleDefault handler:nil]];
+	[actionSheet addAction:[UIAlertAction actionWithTitle:@"( •_•)>⌐■-■" style:UIAlertActionStyleDefault handler:nil]];
+	[actionSheet addAction:[UIAlertAction actionWithTitle:@"(⌐■_■)" style:UIAlertActionStyleDefault handler:nil]];
 	[actionSheet addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
 	[self presentViewController:actionSheet animated:YES completion:nil];
 }
