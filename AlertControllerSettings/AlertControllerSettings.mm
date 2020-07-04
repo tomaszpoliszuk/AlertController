976233173ba1b34ba0b7154927b9b649d1fd55d2
Tomasz Poliszuk <tomaszpoliszuk@users.noreply.github.com>
2020-07-04 08:47:22 +0200

    small formatting fix

--- AlertControllerSettings/AlertControllerSettings.mm
+++ AlertControllerSettings/AlertControllerSettings.mm
@@ -25,15 +25,13 @@
 	return _specifiers;
 }
 
-- (void) showExampleAlert
-{
+- (void)showExampleAlert {
 	UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"(V)(;,;)(V)" message:@"Why Not Zoidberg?" preferredStyle:UIAlertControllerStyleAlert];
 	[alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
 	[alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
 	[self presentViewController:alert animated:YES completion:nil];
 }
-- (void) showExampleActionSheet
-{
+- (void)showExampleActionSheet {
 	UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:@"( ͡° ͜ʖ ͡°)" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
 	[actionSheet addAction:[UIAlertAction actionWithTitle:@"(╯°益°)╯彡┻━┻" style:UIAlertActionStyleDefault handler:nil]];
 	[actionSheet addAction:[UIAlertAction actionWithTitle:@"( ͡° ͜ʖ ͡°) ( ͡⊙ ͜ʖ ͡⊙) ( ͡◉ ͜ʖ ͡◉ )" style:UIAlertActionStyleDefault handler:nil]];
